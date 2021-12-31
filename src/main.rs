92f9e08e6c8c5745db5eb8052d6c396cd47d5a3c
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-31 15:31:06 -0800

    added basic test menu using console and dialogue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use dialoguer::{
+    Select,
+    theme::ColorfulTheme
+};
+use console::Term;
+
+fn main() -> std::io::Result<()> {
+    let items = ["View Expenses", "View Savings", "View Free Spending", "Help"];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact_on_opt(&Term::stderr())?;
+
+        match selection {
+            Some(index) => println!("User selected item : {}", items[index]),
+            None => println!("User did not select anything")
+        }
+
+        Ok(())
 }
