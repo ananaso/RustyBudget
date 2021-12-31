fc70a820807a75c840c91f5da5780aaf4cdf4a3a
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-31 15:50:58 -0800

    added quit option to menu that exits program

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-use dialoguer::{
-    Select,
-    theme::ColorfulTheme
-};
 use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
 
 fn main() -> std::io::Result<()> {
-    let items = ["View Expenses", "View Savings", "View Free Spending", "Help"];
-    let selection = Select::with_theme(&ColorfulTheme::default())
-        .items(&items)
-        .default(0)
-        .interact_on_opt(&Term::stderr())?;
+    loop {
+        let items = [
+            "View Expenses",
+            "View Savings",
+            "View Free Spending",
+            "Help",
+            "Quit",
+        ];
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .items(&items)
+            .default(0)
+            .interact_on_opt(&Term::stderr())?;
 
         match selection {
+            Some(index) if index == 4 => break Ok(()),
             Some(index) => println!("User selected item : {}", items[index]),
-            None => println!("User did not select anything")
+            None => println!("User did not select anything"),
         }
-
-        Ok(())
+    }
 }
