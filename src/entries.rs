61d8d7232664738c4f777deb8f8a552c8457ec7d
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-31 21:37:34 -0800

    removed useless/effort duplicating code

--- src/entries.rs
+++ src/entries.rs
@@ -3,22 +3,8 @@ pub struct Entry {
     pub amount: f32,
 }
 
-fn print_all(entries: &mut Vec<Entry>) {
+pub fn print_all(entries: &mut Vec<Entry>) {
     for entry in entries {
         println!("\t{}: {}", entry.name, entry.amount);
-    }
-}
-
-pub mod expenses {
-    pub fn print_all(expenses: &mut Vec<super::Entry>) {
-        println!("Expenses ->");
-        super::print_all(expenses);
-    }
-}
-
-pub mod savings {
-    pub fn print_all(savings: &mut Vec<super::Entry>) {
-        println!("Savings ->");
-        super::print_all(savings);
     }
 }
