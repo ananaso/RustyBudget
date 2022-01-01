37207cedd094d721ae41067dad23c289024f45d7
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-31 18:02:29 -0800

    updated with generic Entry; prompt remembers last selected

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 use console::Term;
 use dialoguer::{theme::ColorfulTheme, Select};
-use expenses::{print_expenses, Expense};
-mod expenses;
+
+mod entries;
+use entries::{expenses, savings, Entry};
 
 fn main() -> std::io::Result<()> {
     let mut expenses = vec![
-        Expense {
+        Entry {
             name: String::from("apples"),
             amount: 25f32,
         },
-        Expense {
+        Entry {
             name: String::from("oranges"),
             amount: 32.50f32,
         },
     ];
+
+    let mut savings = vec![
+        Entry {
+            name: String::from("Emergency Fund"),
+            amount: 442f32,
+        },
+        Entry {
+            name: String::from("Car"),
+            amount: 450f32,
+        },
+    ];
+
+    let mut last_selected = 0;
 
     loop {
         let items = [
@@ -25,12 +39,17 @@ fn main() -> std::io::Result<()> {
         ];
         let selection = Select::with_theme(&ColorfulTheme::default())
             .items(&items)
-            .default(0)
+            .default(last_selected)
             .interact_on_opt(&Term::stderr())?;
+
+        last_selected = selection.unwrap();
 
         match selection {
             Some(index) if index == items.iter().position(|&x| x == "View Expenses").unwrap() => {
-                print_expenses(&mut expenses)
+                expenses::print_all(&mut expenses)
+            }
+            Some(index) if index == items.iter().position(|&x| x == "View Savings").unwrap() => {
+                savings::print_all(&mut savings)
             }
             Some(index) if index == items.iter().position(|&x| x == "Quit").unwrap() => {
                 break Ok(())
