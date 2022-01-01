62e984e6138e19b2f32e58f831e50eed0fc3b6c2
Alden Davidson <adavidson+github@protonmail.ch>
2021-12-31 17:11:06 -0800

    utilized new expense module, made match more robust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use console::Term;
 use dialoguer::{theme::ColorfulTheme, Select};
+use expenses::{print_expenses, Expense};
+mod expenses;
 
 fn main() -> std::io::Result<()> {
+    let mut expenses = vec![
+        Expense {
+            name: String::from("apples"),
+            amount: 25f32,
+        },
+        Expense {
+            name: String::from("oranges"),
+            amount: 32.50f32,
+        },
+    ];
+
     loop {
         let items = [
             "View Expenses",
@@ -16,7 +29,12 @@ fn main() -> std::io::Result<()> {
             .interact_on_opt(&Term::stderr())?;
 
         match selection {
-            Some(index) if index == 4 => break Ok(()),
+            Some(index) if index == items.iter().position(|&x| x == "View Expenses").unwrap() => {
+                print_expenses(&mut expenses)
+            }
+            Some(index) if index == items.iter().position(|&x| x == "Quit").unwrap() => {
+                break Ok(())
+            }
             Some(index) => println!("User selected item : {}", items[index]),
             None => println!("User did not select anything"),
         }
